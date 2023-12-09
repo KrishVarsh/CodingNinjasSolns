a6fe76174ea81b76dc72544f3a0a2d07ac87f073
Padmakshi V <padmakshihk@gmail.com>
2023-12-09 07:45:08 +0530

    Create  Star Diamond.java

---
+++ Star Diamond.java
@@ -0,0 +1,43 @@
+
+  *
+ ***
+*****
+*****
+ ***
+  *    
+public class Solution {
+    public static void nStarDiamond(int n) {
+        // Write your code here
+         for(int i=0;i<n;i++){
+            //space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //star
+            
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+            
+          for(int i=0;i<n;i++){
+            //space
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            //star
+            
+            for(int j=2*n-(2*i+1);j>=1;j--){
+                System.out.print("*");
+            }
+            //space
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }}}
