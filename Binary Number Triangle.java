2bcb5bc23eb704158c3cdf5ed67fcc05969ac3a3
Padmakshi V <padmakshihk@gmail.com>
2023-12-09 08:05:42 +0530

    Create Binary Number Triangle.java

---
+++ Binary Number Triangle.java
@@ -0,0 +1,24 @@
+// //Binary Number Triangle
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+public class Solution {
+    public static void nBinaryTriangle(int n) {
+        // Write your code here.
+        int start=1;
+        for(int i=1;i<=n;i++){
+             if(i%2!=0) start=1;
+                else start=0;
+            for(int j=1;j<=i;j++){
+               
+                System.out.print(start+" ");
+                start=1-start;
+
+                
+                
+            }
+            System.out.println();
+        }
+    }
+}
