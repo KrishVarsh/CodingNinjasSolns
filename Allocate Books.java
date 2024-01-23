5bd864bb4be3ea5d7f08043e018f84b3668c9b70
Padmakshi V <padmakshihk@gmail.com>
2024-01-23 21:25:40 +0530

    Create  Allocate Books.java

---
+++ Allocate Books.java
@@ -0,0 +1,111 @@
+import java.util.ArrayList;
+
+public class Solution 
+
+{
+
+    boolean isPossible(ArrayList<Integer> arr ,int mid,int m)
+
+    {
+
+       int sum=0;
+
+       int student=1;
+
+       for(int i=0;i<arr.size();i++)
+
+       {
+
+           sum=sum+arr.get(i);
+
+           if(sum>mid)
+
+           {
+
+               student++;
+
+               sum=arr.get(i);
+
+                
+
+           }
+
+       }
+
+       return (student<=m);
+
+    }
+
+    public static int findPages(ArrayList<Integer> arr, int n, int m) 
+
+    {
+
+        // Write your code here.
+
+        Solution s1 = new Solution();  
+
+        int max =0;
+
+        int sum=0;
+
+        int ans=-1;
+
+ 
+
+        if(n<m)
+
+        {
+
+            return ans;
+
+        }
+
+ 
+
+        for(int i=0;i<n;i++)
+
+        {
+
+            sum=sum+arr.get(i);
+
+            max=Math.max(max,arr.get(i));
+
+        }
+
+        int lo=max;
+
+        int ho=sum;
+
+        while(lo<=ho)
+
+        {
+
+            int mid=(lo+ho)/2;
+
+            if(s1.isPossible(arr,mid,m)==true)
+
+            {
+
+                ans=mid;
+
+                ho=mid-1;
+
+            }
+
+            else
+
+            {
+
+                 lo=mid+1;
+
+            }
+
+        }
+
+             return ans ;
+
+    }
+
+    
+
+}
